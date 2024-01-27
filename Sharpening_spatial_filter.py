79f9efc5c5c8d28e6b5aedcd45f6055e9cce98d1
Sanjitha Jaggumantri <108885162+sanjitha-patnaik@users.noreply.github.com>
2024-01-27 17:24:51 +0530

    Update Sharpening_spatial_filter.py

--- Sharpening_spatial_filter.py
+++ Sharpening_spatial_filter.py
@@ -1,1 +1,14 @@
+import cv2
+import numpy as np
+image = cv2.imread(r"image_path")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+laplacian = cv2.Laplacian(gray_image, cv2.CV_64F)
+laplacian = np.uint8(np.absolute(laplacian))
+sharpened_image = cv2.addWeighted(gray_image, 1.5, laplacian, -0.5, 0)
+output_image = cv2.merge((sharpened_image, sharpened_image, sharpened_image))
 
+# Save and display the output sharpened color image
+cv2.imwrite('output_sharpened_color_image.jpg', output_image)
+cv2.imshow('Output Sharpened Color Image', output_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
